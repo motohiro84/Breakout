a15ebe6e365e86f1a9f0a9649f2f11c5b6cf5a41
motohiro84 <motohiro84@users.noreply.github.com>
2020-09-23 17:54:11 +0900

    効果音

---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Sound : MonoBehaviour
+{
+    public AudioClip sound01;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter()
+    {
+        AudioSource.PlayClipAtPoint(sound01, Camera.main.transform.position);
+    }
+}
