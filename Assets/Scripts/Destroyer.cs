5e457612d9d847ad054b0c655da5662f6ab88d3e
motohiro84 <motohiro84@users.noreply.github.com>
2020-09-25 10:58:38 +0900

    タイトルシーンの変更

--- Assets/Scripts/Destroyer.cs
+++ Assets/Scripts/Destroyer.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Destroyer : MonoBehaviour
 {
+
 
     public GameObject masterObj;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(SceneManager.GetActiveScene().name == "Hard")
+        {
+            
+        }
     }
 
     // Update is called once per frame
